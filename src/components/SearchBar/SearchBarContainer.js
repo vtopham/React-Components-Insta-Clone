7e40246daffe1f35a0f68558fafa55a5adf2932b
Victoria  Topham <topham.victoria@gmail.com>
2020-03-17 09:12:07 -0700

    fixed the icons not loading and updated the search bar to match

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -6,7 +6,8 @@ const SearchBar = () => {
   return (
     <div className="search-bar-wrapper">
       <div className="image-wrapper">
-        <i className="fab fa-instagram" />
+        <i className="fa fa-instagram" />
+        <img className = "logo-image" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt= "instagram logo"/>
       </div>
       <form className="search-form">
         <input
@@ -16,13 +17,13 @@ const SearchBar = () => {
       </form>
       <div className="social-wrapper">
         <div className="social">
-          <i className="far fa-compass" />
+          <i className="fa fa-compass" />
         </div>
         <div className="social">
-          <i className="far fa-heart" />
+          <i className="fa fa-heart" />
         </div>
         <div className="social">
-          <i className="far fa-user-circle" />
+          <i className="fa fa-user-circle" />
         </div>
       </div>
     </div>
