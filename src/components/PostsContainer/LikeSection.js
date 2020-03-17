22b0e0640a50d2369825d9a177ecf4dad814060d
Victoria  Topham <topham.victoria@gmail.com>
2020-03-17 14:46:33 -0700

    made it so you can only like once

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,15 +1,25 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, {useState} from "react";
 
 const LikeSection = props => {
 
   const {postLikes, setPostLikes} = props;
 
+  const [isLiked, setIsLiked] = useState(0);
+
   function addALike(e) {
-    setPostLikes(postLikes + 1);
+    if (isLiked === 0) {
+      setIsLiked(1);
+      setPostLikes(postLikes + 1);
+    } else {
+      setIsLiked(0);
+      setPostLikes(postLikes - 1);
+    }
+    
   }
+
   return (
     <div>
     <div
